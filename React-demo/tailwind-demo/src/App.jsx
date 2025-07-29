79916ae0110e5159544285538e13dc891ea8c172
dnjsxoghd <feelchok1234@gmail.com>
2025-07-30 00:43:33 +0900

    tailwind concepts and apply

---
+++ React-demo/tailwind-demo/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <section className="flex justify-center mr-4 ml-4 gap-10 text-center">
+        <div className="w-full border-[1px]">1</div>
+        <div className="w-full border-[1px]">2</div>
+      </section>
+    </>
+  );
+}
+
+export default App;
